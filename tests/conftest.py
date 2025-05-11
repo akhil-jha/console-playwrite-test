e075823fc52e6663cfd1604bbef378d060ee219c
Akhil Jha <akjha@redhat.com>
2025-05-12 00:50:38 +0530

    Initial Commit

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from dataclasses import dataclass
+from dynaconf import Dynaconf
+
+settings = Dynaconf(
+    settings_files=["conf/settings.yaml"],
+    environments=True,
+    envvar_prefix="DYNACONF",
+    load_dotenv=True
+
+)
+
+@dataclass
+class Config:
+    base_url: str = settings.base_url
+    username: str = settings.username
+    password: str = settings.password
+    headless: bool = settings.headless
+
+@pytest.fixture(scope="session")
+def config() -> Config:
+    return Config()
