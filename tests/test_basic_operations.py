b560f360cc48653314b90e62d0c3235e0888c3a5
Akhil Jha <akjha@redhat.com>
2025-05-12 10:07:59 +0530

    Add pre-commit

--- tests/test_basic_operations.py
+++ tests/test_basic_operations.py
@@ -1,6 +1,3 @@
-import pytest
-from fixtures.login import login_session, login_function
-
 def test_login(login_function):
     login_function.wait_for_timeout(10_000)
     login_function.screenshot(path="loggedin.png")
