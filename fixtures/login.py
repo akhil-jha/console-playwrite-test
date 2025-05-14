577496db7c962e821c6befc004acd5635abcd7b7
Akhil Jha <akjha@redhat.com>
2025-05-14 22:02:11 +0530

    Remove consent checkup

--- fixtures/login.py
+++ fixtures/login.py
@@ -11,7 +11,6 @@ from playwright.sync_api import Page
 def login_console(page: Page, config: Config):
     page.goto(config.base_url)
     expect(page).to_have_title(re.compile("Log In | Red Hat IDP"))
-    page.locator("#truste-consent-buttons").click()
     page.locator('input[id="username-verification"]').fill(config.username)
     page.get_by_role("button", name="Next").click()
     page.locator('input[id="password"]').fill(config.password)
