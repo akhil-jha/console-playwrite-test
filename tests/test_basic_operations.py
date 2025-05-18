c364a3f6897b1b0b2c2a9b864a7c32257104c331
Akhil Jha <38880644+akhil-jha@users.noreply.github.com>
2025-05-18 18:49:43 +0530

    changed

--- tests/test_basic_operations.py
+++ tests/test_basic_operations.py
@@ -1,3 +1,3 @@
 def test_screenshot(login_console):
     login_console.wait_for_timeout(10_000)
-    login_console.screenshot(path="loggedin.png")
+    login_console.screenshot(path="logged-in.png")
